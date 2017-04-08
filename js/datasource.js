c33ad359b8814baee60f15a40bf05d63934a2a7f
qbian61 <15620608572@163.com>
2017-04-08 22:40:47 +0800

    add blog

---
+++ js/datasource.js
@@ -0,0 +1,122 @@
+'use strict'
+
+window.qbian = {};
+
+(function(qbian) {
+
+  // sources
+
+  qbian.sources = [
+    {
+      source : {
+        title : 'spring MVC 的 web.xml 配置讲解',
+        href : 'http://blog.csdn.net/qbian/article/details/53431813'
+      },
+      tags : ['Spring', 'Java'],
+    },{
+      source : {
+        title : 'MySQL 基础命令',
+        href : 'http://blog.csdn.net/qbian/article/details/53432239'
+      },
+      tags : ['MySQL'],
+    },{
+      source : {
+        title : 'JVM 多线程的内存模型',
+        href : 'http://blog.csdn.net/qbian/article/details/53612124'
+      },
+      tags : ['Java', 'JVM'],
+    },{
+      source : {
+        title : 'synchronized、volatile、Atomic区别和用法',
+        href : 'http://blog.csdn.net/qbian/article/details/53736118'
+      },
+      tags : ['Java'],
+    },{
+      source : {
+        title : 'JavaScript原型',
+        href : 'http://blog.csdn.net/qbian/article/details/53813106'
+      },
+      tags : ['JavaScript'],
+    },{
+      source : {
+        title : 'Servlet修改编码无效的问题',
+        href : 'http://blog.csdn.net/qbian/article/details/53905780'
+      },
+      tags : ['Java'],
+    },{
+      source : {
+        title : 'java web实现Gzip压缩传输',
+        href : 'http://blog.csdn.net/qbian/article/details/53909778'
+      },
+      tags : ['Java'],
+    },{
+      source : {
+        title : 'AngularJs的服务及其包装器讲解',
+        href : 'http://blog.csdn.net/qbian/article/details/53943822'
+      },
+      tags : ['AngularJs', 'JavaScript'],
+    },{
+      source : {
+        title : 'Nginx基本配置讲解',
+        href : 'http://blog.csdn.net/qbian/article/details/54290588'
+      },
+      tags : ['Nginx'],
+    },{
+      source : {
+        title : 'Nginx负载均衡配置',
+        href : 'http://blog.csdn.net/qbian/article/details/69267045'
+      },
+      tags : ['Nginx'],
+    },{
+      source : {
+        title : 'java编写异步处理方法提升系统性能',
+        href : 'http://blog.csdn.net/qbian/article/details/69261853'
+      },
+      tags : ['Java', 'Spring', 'Spring Boot'],
+    },{
+      source : {
+        title : 'Spring Boot打包jar或war',
+        href : 'http://blog.csdn.net/qbian/article/details/68559761'
+      },
+      tags : ['Java', 'Spring', 'Spring Boot'],
+    },{
+      source : {
+        title : 'JavaScript模块化规范commonJs、AMD、CMD',
+        href : 'http://blog.csdn.net/qbian/article/details/54378106'
+      },
+      tags : ['JavaScript'],
+    }
+  ];
+
+  // tags
+
+  qbian.tags = [
+    {
+      name : 'Java',
+      sources : []
+    }, {
+      name : 'JVM',
+      sources : []
+    }, {
+      name : 'JavaScript',
+      sources : []
+    }, {
+      name : 'Spring',
+      sources : []
+    }, {
+      name : 'Spring Boot',
+      sources : []
+    }, {
+      name : 'AngularJs',
+      sources : []
+    }, {
+      name : 'MySQL',
+      sources : []
+    }, {
+      name : 'Nginx',
+      sources : []
+    }
+  ];
+
+
+})(window.qbian);
