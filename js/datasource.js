afea7c47a60febbff7eb63a28722724e709e6f60
qbian61 <15620608572@163.com>
2017-04-15 19:02:31 +0800

    如何保证RabbitMQ的消息不丢失

--- js/datasource.js
+++ js/datasource.js
@@ -9,6 +9,12 @@ window.qbian = {};
   qbian.sources = [
     {
       source : {
+        title : '如何保证RabbitMQ的消息不丢失',
+        href : 'http://www.jianshu.com/p/558c4b6b8d26'
+      },
+      tags : ['MQ'],
+    },{
+      source : {
         title : 'AngularJs的controller 多次执行问题',
         href : 'http://blog.csdn.net/qbian/article/details/70139381'
       },
